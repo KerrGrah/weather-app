2b6308208dde40b1358b8b54502807e4d48eb0d5
Graham <grey.ham.acre@gmail.com>
2018-02-17 01:51:12 +0300

    fixed error in action

--- src/actions/userInputChange.js
+++ src/actions/userInputChange.js
@@ -2,6 +2,4 @@ import { makeActionCreator } from "./makeActionCreator";
 export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
 export const USER_INPUT_EMPTY = "USER_INPUT_EMPTY";
 
-export const userInputChange = function() {
-  makeActionCreator(USER_INPUT_CHANGE, "input");
-};
+export const userInputChange = makeActionCreator(USER_INPUT_CHANGE, "input");
