3d67507c0a4cab786398ffb6f33daef342ab9e02
Graham <grey.ham.acre@gmail.com>
2018-02-20 17:07:29 +0300

    Add config file to repository

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const citiesByMatchUrl = "http://localhost:4000/match/";
+export const citiesByCountryUrl = "http://localhost:4000/country/";
+export const apiKey = `d50c997214de3ac0053a3bbac58101a9`;
